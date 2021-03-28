b2ba7bfa6cbe009c70c0d9c97d327f5ec9cf4a89
Daniele De Francesco <93.daniele@gmail.com>
2021-03-28 12:34:15 +0200

    Added unit tests for ProductProfile

--- test/unit/AspNetCore.Examples.ProductService.Test/Profiles/ProductProfileTest.cs
+++ test/unit/AspNetCore.Examples.ProductService.Test/Profiles/ProductProfileTest.cs
@@ -1,7 +1,52 @@
+using AspNetCore.Examples.ProductService.Entities;
+using AspNetCore.Examples.ProductService.ValueObjects;
+using AutoMapper;
+using FluentAssertions;
+using NUnit.Framework;
+
 namespace AspNetCore.Examples.ProductService.Profiles
 {
-    public class ProductProfileTest
+    public class ProductProfileTest : ProfileTestBase<ProductProfile>
     {
+        [Test]
+        public void Product_ToProductDto_IsMappedCorrectly()
+        {
+            var product = new Product()
+            {
+                Id = "Id",
+                Name = ProductName.From("Name")
+            };
+
+            var productDto = _mapper.Map<ProductDto>(product);
+
+            productDto.Id
+                .Should()
+                .Be(product.Id);
+            
+            productDto.Name
+                .Should()
+                .Be(product.Name.Value);
+        }
         
+        [Test]
+        public void ProductDto_ToProduct_IsMappedCorrectly()
+        {
+            var productDto = new ProductDto()
+            {
+                Id = "Id",
+                Name = "Name"
+            };
+
+            var product = _mapper.Map<Product>(productDto);
+
+            product.Id
+                .Should()
+                .Be(productDto.Id);
+            
+            product.Name
+                .Should()
+                .Be(ProductName.From(productDto.Name));
+        }
+
     }
 }
--- test/unit/AspNetCore.Examples.ProductService.Test/Profiles/ProfileTestBase.cs
+++ test/unit/AspNetCore.Examples.ProductService.Test/Profiles/ProfileTestBase.cs
@@ -1,4 +1,25 @@
-$HEADER$namespace $NAMESPACE$
+using AutoMapper;
+using NUnit.Framework;
+
+namespace AspNetCore.Examples.ProductService.Profiles
 {
-  public class $CLASS$ {$END$}
+    public abstract class ProfileTestBase<T> where T : Profile, new()
+    {
+        protected MapperConfiguration _mapperConfiguration;
+        protected IMapper _mapper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mapperConfiguration = new MapperConfiguration(
+                cfg => cfg.AddProfile<T>());
+            _mapper = _mapperConfiguration.CreateMapper();
+        }
+
+        [Test]
+        public void Configuration_IsValid()
+        {
+            _mapperConfiguration.AssertConfigurationIsValid();
+        }
+    }
 }
