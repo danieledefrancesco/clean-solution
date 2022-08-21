925dfa6ef68fcdf3d75a9e6842f4b4567752d31c
De Francesco, Daniele <daniele11.defrancesco@nttdata.com>
2022-08-21 18:57:32 +0200

    fix: fix HttpClient injection in PiceCardServiceClient

--- app/test/unit/AspNetCore.Examples.ProductService.Infrastructure.Test/Factories/PriceCardServiceClientFactoryTest.cs
+++ app/test/unit/AspNetCore.Examples.ProductService.Infrastructure.Test/Factories/PriceCardServiceClientFactoryTest.cs
@@ -12,26 +12,23 @@ namespace AspNetCore.Examples.ProductService.Factories
         private PriceCardServiceClientFactory _priceCardServiceClientFactory;
         private IOptions<PriceCardServiceClientConfiguration> _options;
         private PriceCardServiceClientConfiguration _priceCardServiceClientConfiguration;
-        private IHttpClientFactory _httpClientFactory;
+        private HttpClient _httpClient;
 
         [SetUp]
         public void SetUp()
         {
             _options = Substitute.For<IOptions<PriceCardServiceClientConfiguration>>();
-            _httpClientFactory = Substitute.For<IHttpClientFactory>();
+            _httpClient = Substitute.For<HttpClient>();
             
             _priceCardServiceClientConfiguration = new PriceCardServiceClientConfiguration();
             _options.Value.Returns(_priceCardServiceClientConfiguration);
             
-            _priceCardServiceClientFactory = new PriceCardServiceClientFactory(_options, _httpClientFactory);
+            _priceCardServiceClientFactory = new PriceCardServiceClientFactory(_options, _httpClient);
         }
 
         [Test]
         public void Create_ReturnsPriceCardServiceClient()
         {
-            var httpClient = Substitute.For<HttpClient>();
-            _httpClientFactory.CreateClient().Returns(httpClient);
-            
             _priceCardServiceClientConfiguration.PriceCardServiceBaseUri = "https://test.com/";
 
             var priceCardServiceClient = _priceCardServiceClientFactory.Create();
@@ -39,7 +36,6 @@ namespace AspNetCore.Examples.ProductService.Factories
             priceCardServiceClient.Should().NotBeNull();
             priceCardServiceClient.BaseUrl.Should().Be(_priceCardServiceClientConfiguration.PriceCardServiceBaseUri);
 
-            _httpClientFactory.CreateClient().Received(1);
         }
     }
 }
