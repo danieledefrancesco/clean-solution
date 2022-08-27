817015b0b0460be89b583e891912b2d3961172f8
De Francesco, Daniele <daniele11.defrancesco@nttdata.com>
2022-08-27 14:58:16 +0200

    feature: add fluent validation rules in swagger file

--- app/src/AspNetCore.Examples.ProductService/Program.cs
+++ app/src/AspNetCore.Examples.ProductService/Program.cs
@@ -6,6 +6,7 @@ using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Swashbuckle.AspNetCore.Swagger;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,7 +15,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+  options.AddFluentValidationRulesScoped();  
+});
 builder.Services
     .AddControllers()
     .AddFluentValidation(fv =>
